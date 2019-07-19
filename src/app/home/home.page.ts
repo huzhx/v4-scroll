aa3d749bac1eb34776188f8663e0b48ba3da699e
Zhaoxian Hu <huzhx@uci.edu>
2019-07-19 16:59:09 -0700

    detect if the page is overflown

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,22 +7,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   isAtBottom: boolean;
+  isOverflown: boolean;
 
   constructor() {
     this.isAtBottom = false;
+  }
+
+  ionViewDidEnter() {
+    this.isOverflown = this.checkOverflown();
+    console.log(this.isOverflown);
   }
 
   getContent() {
     return document.querySelector('ion-content');
   }
 
+  getTextContent() {
+    return document.querySelector('div');
+  }
+
   scrollToBottom() {
-    this.getContent().scrollToBottom(500);
+    this.getContent().scrollToBottom(1000);
     this.isAtBottom = true;
   }
 
   scrollToTop() {
-    this.getContent().scrollToTop(500);
+    this.getContent().scrollToTop(1000);
     this.isAtBottom = false;
+  }
+
+  checkOverflown() {
+    console.log(this.getContent().scrollHeight);
+    console.log(this.getContent().clientHeight);
+    console.log(this.getContent().offsetHeight);
+    console.log(this.getTextContent().scrollHeight);
+    console.log(this.getTextContent().clientHeight);
+    console.log(this.getTextContent().offsetHeight);
+    return this.getTextContent().clientHeight > this.getContent().clientHeight;
   }
 }
