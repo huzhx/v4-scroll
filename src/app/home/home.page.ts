3fd6fbd139b1f8cc4b611afe99fc04d932920415
Zhaoxian Hu <huzhx@uci.edu>
2019-07-19 15:29:07 -0700

    add a simple fab to scroll down/up

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  isAtBottom: boolean;
 
-  constructor() {}
+  constructor() {
+    this.isAtBottom = false;
+  }
 
+  getContent() {
+    return document.querySelector('ion-content');
+  }
+
+  scrollToBottom() {
+    this.getContent().scrollToBottom(500);
+    this.isAtBottom = true;
+  }
+
+  scrollToTop() {
+    this.getContent().scrollToTop(500);
+    this.isAtBottom = false;
+  }
 }
